2ce941c95d5734d2f4cd1c63b2267aa4d75ef666
KlimovaED <klimovaed@icloud.com>
2024-07-17 02:01:07 +0200

    17.07

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
 import {UserType} from '../HW8'
 
+
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to
+    // fix any
     switch (action.type) {
         case 'sort': { // by name
+            let newCopy= state;
+            newCopy.sort((a,b)=>(action.payload==='up')? (a.age>b.age? 1: -1): (a.age<b.age? 1: -1));
+                return newCopy
 
-            return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+           const newFilter= state.filter(st=>st.age >= action.payload)
+            return newFilter // need to fix
         }
         default:
             return state
     }
 }
+
+
